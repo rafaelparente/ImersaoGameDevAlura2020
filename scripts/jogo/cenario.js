bd2142d1424e087b1ccec7a6c420305236125890
Rafael Parente <rafaelp.dev@gmail.com>
2020-06-25 11:45:01 -0300

    Class 'Cenario' refactored.

--- scripts/jogo/cenario.js
+++ scripts/jogo/cenario.js
@@ -1,4 +1,5 @@
 class Cenario {
+
     constructor(imagem, velocidade) {
       this.imagem = imagem;
       this.velocidade = velocidade;
@@ -15,13 +16,13 @@ class Cenario {
       this.x1 = this.x1 - this.velocidade;
       this.x2 = this.x2 - this.velocidade;
   
-      if (this.x1 < -width) {
+      if (this.x1 <= -width) {
         this.x1 = width;
       }
-  
-      if (this.x2 < -width) {
+      else if (this.x2 <= -width) {
         this.x2 = width;
       }
     }
+
   }
   
