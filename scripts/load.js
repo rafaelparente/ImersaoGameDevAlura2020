ea2ca11f47709ab0b9676e937203e9d441eeaca2
Rafael Parente <rafaelp.dev@gmail.com>
2020-06-29 20:25:23 -0300

    Fix assets location

--- scripts/load.js
+++ scripts/load.js
@@ -1,16 +1,16 @@
 function preload() {
-    imagemTelaInicial = loadImage('../imagens/assets/telaInicial.png');
-    imagemTelaFinal = loadImage('../imagens/assets/game-over.png');
-    imagemCenario = loadImage('../imagens/cenario/floresta.png');
-    imagemPersonagem = loadImage('../imagens/personagem/correndo.png');
-    imagemInimigo = loadImage('../imagens/inimigos/gotinha.png');
-    imagemInimigoGrande = loadImage('../imagens/inimigos/troll.png');
-    imagemInimigoVoador = loadImage('../imagens/inimigos/gotinha-voadora.png');
+    imagemTelaInicial = loadImage('./imagens/assets/telaInicial.png');
+    imagemTelaFinal = loadImage('./imagens/assets/game-over.png');
+    imagemCenario = loadImage('./imagens/cenario/floresta.png');
+    imagemPersonagem = loadImage('./imagens/personagem/correndo.png');
+    imagemInimigo = loadImage('./imagens/inimigos/gotinha.png');
+    imagemInimigoGrande = loadImage('./imagens/inimigos/troll.png');
+    imagemInimigoVoador = loadImage('./imagens/inimigos/gotinha-voadora.png');
 
-    somDoJogo = loadSound('../sons/trilha_jogo.mp3');
-    somDoPulo = loadSound('../sons/somPulo.mp3');
+    somDoJogo = loadSound('./sons/trilha_jogo.mp3');
+    somDoPulo = loadSound('./sons/somPulo.mp3');
 
-    const fonte= loadFont('../imagens/assets/fonteTelaInicial.otf');
+    const fonte= loadFont('./imagens/assets/fonteTelaInicial.otf');
     fonteTelaInicial = fonte;
     fonteTelaFinal = fonte;
 }
